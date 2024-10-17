d433739206a3d8909d0066228cadf63c141d1877
beuzathor <christophe1500@gmail.com>
2024-10-17 18:28:59 +0200

    Update Test.php

--- app/Filament/Pages/Test.php
+++ app/Filament/Pages/Test.php
@@ -12,37 +12,26 @@ use FilamentTiptapEditor\TiptapEditor;
 use Filament\Forms\Form;
 use FilamentTiptapEditor\Enums\TiptapOutput;
 
-class Test extends Page implements Forms\Contracts\HasForms 
+class Test extends Page implements Forms\Contracts\HasForms
 {
-    use Forms\Concerns\InteractsWithForms; 
+    use Forms\Concerns\InteractsWithForms;
 
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     protected static string $view = 'filament.pages.test';
 
     public ?array $data = [];
-    
+    public $textjson = '';
+
     public function form(Form $form): Form
     {
         return $form
             ->schema([
-                Builder::make('blocks')
-                    ->label('Bloques')
-                    ->blockPreviews(areInteractive: true)
-                    ->blocks([
-                        Block::make('blockName')
-                            ->schema([
-                                TiptapEditor::make('text')
-                                ->output(TiptapOutput::Json) 
-                            
-                            ])
-                            ->preview('blocks.example')
-                    ])
-                    ->columnSpan('full')
-                    ->cloneable()
-                    ->collapsible(),
-            ])
-            ->statePath('data');
+                    TiptapEditor::make('textjson')
+
+                        ->maxContentWidth('full')
+                        ->output(TiptapOutput::Json),
+            ]);
     }
-    
+
 }
